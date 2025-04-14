e2eec5261617a9502f7e737c566cb908a4e96969
zlqlpp <zlqlpp@gmail.com>
2025-04-14 23:52:57 +0800

    完整代码，待测试

---
+++ windows内核编程(华章出版)/04_driver/Booster/Booster.cpp
@@ -0,0 +1,33 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include "..\04_driver\PriorityBoosterCommon.h"
+
+int Error(const char* message) {
+    printf("%s (error=%d)\n", message, GetLastError());
+    return 1;
+}
+
+int main(int argc, const char* argv[]) {
+    if (argc < 3) {
+        printf("Usage: Booster <threadid> <priority>\n");
+        return 0;
+    }
+
+    HANDLE hDevice = CreateFile(L"\\\\.\\PriorityBooster", GENERIC_WRITE, FILE_SHARE_WRITE,
+        nullptr, OPEN_EXISTING, 0, nullptr);
+    if (hDevice == INVALID_HANDLE_VALUE)
+        return Error("Failed to open device");
+
+    ThreadData data;
+    data.ThreadId = atoi(argv[1]);
+    data.Priority = atoi(argv[2]);
+
+    DWORD returned;
+    BOOL success = DeviceIoControl(hDevice, IOCTL_PRIORITY_BOOSTER_SET_PRIORITY, &data, sizeof(data), nullptr, 0, &returned, nullptr);
+    if (success)
+        printf("Priority change succeeded!\n");
+    else
+        Error("Priority change failed!");
+
+    CloseHandle(hDevice);
+}
